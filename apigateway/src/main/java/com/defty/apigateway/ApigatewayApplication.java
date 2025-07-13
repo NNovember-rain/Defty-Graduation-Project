e2184477d9262398dc193665747b84fb51b18c33
HiepHD <hiephh6663@gmail.com>
2025-07-13 20:28:40 +0700

    config: discovery service

--- apigateway/src/main/java/com/defty/apigateway/ApigatewayApplication.java
+++ apigateway/src/main/java/com/defty/apigateway/ApigatewayApplication.java
@@ -2,10 +2,12 @@ package com.defty.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class ApigatewayApplication {
 
 	public static void main(String[] args) {
