419a3ef5e77d5abcd2115bb3a701a9d5043b5c44
HiepHD <hiephh6663@gmail.com>
2025-07-14 00:01:17 +0700

    config: setup 3 services: submission,ai-feedback,grading

---
+++ services/ai-feedback-service/src/main/java/com/ai_feedback_service/AiFeedbackServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ai_feedback_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiFeedbackServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiFeedbackServiceApplication.class, args);
+	}
+
+}
---
+++ services/ai-feedback-service/src/test/java/com/ai_feedback_service/AiFeedbackServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai_feedback_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiFeedbackServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
