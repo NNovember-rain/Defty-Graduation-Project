257a9a9d4cc11a53ac5d57303b6e9e48d966f072
TuiTenVanw <nguyenvietvan223@gmail.com>
2025-07-29 00:12:13 +0700

    feat: front end UserPage, RolePage and PermissionPage

--- Front-end/src/shared/services/roleService.ts
+++ Front-end/src/shared/services/roleService.ts
@@ -1,5 +1,6 @@
 import handleRequest from "./handleRequest.ts";
 import {getWithParams} from "./getWithParams.ts";
+import {del, get, patchJsonData, postJsonData} from "./request.ts";
 
 const PREFIX_ROLES = import.meta.env.VITE_PREFIX_ROLES as string;
 const PREFIX_IDENTITY: string = import.meta.env.VITE_PREFIX_IDENTITY as string;
@@ -24,7 +25,14 @@ export interface IRole {
     name: string;
     description: string;
     createdDate: string;
+    isActive: boolean;
+    permissions?: string[];
 }
+
+export const createRole = async (data: Omit<IRole, '_id' | 'createdAt' | 'updatedAt'>): Promise<IRole> => {
+    const response = await handleRequest(postJsonData(`${PREFIX_IDENTITY}/${PREFIX_ROLES}`, data));
+    return await response.json() as Promise<IRole>;
+};
 
 export const getRoles = async (options: GetRolesOptions = {}): Promise<GetRolesResult> => {
     const params = {
@@ -35,7 +43,6 @@ export const getRoles = async (options: GetRolesOptions = {}): Promise<GetRolesR
         sortOrder: options.sortOrder,
     };
 
-    // Sử dụng getWithParams để xây dựng URL và gửi request
     const response = await handleRequest(getWithParams(`${PREFIX_IDENTITY}/${PREFIX_ROLES}`, params));
     const data = await response.json();
     return {
@@ -44,5 +51,31 @@ export const getRoles = async (options: GetRolesOptions = {}): Promise<GetRolesR
         // page: data.result.number - 1,
         limit: data.result.size
     } as GetRolesResult;
+};
+
+export const getRoleById = async (id: string | number): Promise<IRole> => {
+    const response = await handleRequest(get(`${PREFIX_IDENTITY}/${PREFIX_ROLES}/${id}`));
+    const data = await response.json();
+    return data.data as IRole;
+};
+
+export const updateRole = async (id: string | number, data: Partial<Omit<IRole, '_id' | 'createdAt' | 'updatedAt'>>): Promise<IRole> => {
+    const response = await handleRequest(patchJsonData(`${PREFIX_IDENTITY}/${PREFIX_ROLES}/${id}`, data));
+    const updatedData = await response.json();
+    return updatedData.data as IRole;
+};
+
+export const deleteRole = async (id: number): Promise<IRole> => {
+    const response = await handleRequest(del(`${PREFIX_IDENTITY}/${PREFIX_ROLES}/${id}`));
+    const deletedData = await response.json();
+    return deletedData.data as IRole;
+};
+
+export const toggleRoleActiveStatus = async (id: number, isActive: boolean): Promise<IRole> => {
+    const response = await handleRequest(
+        patchJsonData(`${PREFIX_IDENTITY}/${PREFIX_ROLES}/${id}/toggle-active`, { isActive })
+    );
+    const updatedData = await response.json();
+    return updatedData.data as IRole;
 };
 
